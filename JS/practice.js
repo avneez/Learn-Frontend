161f2c217e5a33c4e7bd6b07e3ac616c1166d747
abhargkeshari <abhargkeshari@deqode.com>
2023-01-30 17:26:07 +0530

    24-jan

---
+++ JS/practice.js
@@ -0,0 +1,40 @@
+//REVERSE A NUMBER
+// function rev(num){
+//    var s=num.toString()
+//     temp="";
+//     for(let i=s.length-1; i>=0; i--){
+//         temp+=s[i];
+//     }
+//     num=parseInt(s);
+//     return num;
+// }
+
+// let num1=1234
+// let ans = rev(num1);
+// console.log(ans)
+
+
+//sum 0
+// const arr = [-4, 4, 3, 5, 6, 2, -2];
+
+// function zeroSum(arr) {
+//     const ans = [];
+
+//     let n = arr.length;
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             if (arr[i] + arr[j] == 0) {
+//                 ans.push(arr[i]);
+//                 ans.push(arr[j]);
+//                 return ans;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// let res = zeroSum(arr);
+// console.log(res)
+
+
+//
