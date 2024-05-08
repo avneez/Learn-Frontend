c642426b900fa1048bc296c6ab6e3c6d878e5fc5
avneez <abhargkeshari@deqode.com>
2024-05-08 14:24:32 +0530

    Added LazyLoading to the productsListing component

--- fullstack/learningApp/frontend/src/App.jsx
+++ fullstack/learningApp/frontend/src/App.jsx
@@ -1,40 +1,14 @@
+import React from 'react'
 import './App.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { Suspense } from 'react'
+const ProductsListing = React.lazy(() => import('./pages/ProductsListing'))
 
 function App() {
-  const [products, setProducts] = useState([])
-
-  const url = "http://localhost:3000"
-
-  const fetchData = () => {
-    axios.get(`${url}/api/products`)
-      .then((response) => setProducts(response.data.data))
-      .catch((error) => console.error(error))
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
-    <>
-      <div>
-        <h3>My Products ({products.length})</h3>
-      </div>
-      {!(products.length > 0) && <div>No Products  Yet...</div>}
-      {
-        products.length > 0 &&
-        products.map((product, index) => {
-          return (
-            <div key={product.id} style={{ width: "100%", borderBottom: "solid" }}>
-              <div>{`${index+1}. ${product.name} - ${product.price}`}</div>
-              <div>{product.description}</div>
-            </div>
-          )
-        })
-      }
-    </>
+    <Suspense fallback={<p>Loading products...</p>}>
+      <ProductsListing/>
+    </Suspense>
   )
 }
 
